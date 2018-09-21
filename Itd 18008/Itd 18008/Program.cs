186856fc084eff80845e14c751e0f3a2e47aca02
bhupinderrandhawa76 <43333988+bhupinderrandhawa76@users.noreply.github.com>
2018-09-21 10:42:53 +0530

    Add files via upload

---
+++ Itd 18008/Itd 18008/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itd_18008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 15.5;
+            double num2 = 16.5;
+            double num3 = 17.5;
+            double result = (num1 + num2 + num3) / 3;
+            Console.WriteLine("Average of "+num1+","+num2+" and "+num3+" is " + result);
+        }
+    }
+}
